cee90c329fb8feaf466cb7328aee39d123dcd270
François Beausoleil <francois@teksol.info>
2018-03-22 23:30:48 -0400

    Use the same sort key for products in all lists

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,16 @@ class Product < ApplicationRecord
   belongs_to :group
   has_many :reservations
 
-  scope :by_name, ->{ order(Arel.sql("LOWER(name)")) }
+  scope :by_name, ->{ order(Arel.sql("LOWER(name), id")) }
   scope :search, ->(string){ where(Arel.sql("POSITION(? IN name || ' ' || COALESCE(description, '')) > 0", string)) }
   scope :with_reservations, ->{ includes(reservations: :event) }
 
   validates :name, presence: true, length: { minimum: 2 }
+
+  # Returns a string to be used for ordering a list of products consistently
+  #
+  # @return [String] The key to use to order this item consistently
+  def sort_key
+    [name.downcase, id].map(&:to_s).join(":")
+  end
 end
