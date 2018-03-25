45bbbef556a03c0c32df9ad9bdb31cec4715838f
François Beausoleil <francois@teksol.info>
2018-03-24 21:44:38 -0400

    Improve quality of life for new developers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+tentes      = Category.create(name: "Tentes")
+cuisine     = Category.create(name: "Cuisine")
+chauffage   = Category.create(name: "Chauffage")
+
+fleurimont  = Group.create!(name: "10ème Groupe Scout Est-Calade")
+francois    = fleurimont.users.create!(email: "francois@teksol.info",  name: "Le Chouette Baloo", password: "monkey", password_confirmation: "monkey")
+claudette   = fleurimont.users.create!(email: "claudette@teksol.info", name: "Claudette",         password: "monkey", password_confirmation: "monkey")
+
+tente1 = fleurimont.products.create!(name: "Tente 4x5 6 personnes (#1)")
+tente1.categories = [ tentes ]
+
+tente2 = fleurimont.products.create!(name: "Tente 4x5 6 personnes (#2)")
+tente1.categories = [ tentes ]
+
+pot_lait1 = fleurimont.products.create!(name: "Pot à lait #1")
+pot_lait1.categories = [ cuisine ]
+
+pot_lait2 = fleurimont.products.create!(name: "Pot à lait #1")
+pot_lait2.categories = [ cuisine ]
+
+rond_de_feu = fleurimont.products.create!(name: "Rond de feu")
+rond_de_feu.categories = [ chauffage, cuisine ]
+
+rock_forest = Group.create!(name: "47ème Groupe Scout Rock-Forest")
+tenterf1    = rock_forest.products.create!(name: "Tente prospecteur #1")
+tenterf2    = rock_forest.products.create!(name: "Tente prospecteur #2")
+
+st_elie     = Group.create!(name: "48ème Groupe Scout Les Rassembleurs")
