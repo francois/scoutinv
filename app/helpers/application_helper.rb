53579457028467ac1b778919123c31e3cded6c1f
François Beausoleil <francois@teksol.info>
2018-08-21 10:53:09 -0400

    Better display dates for one day events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,9 @@ module ApplicationHelper
     first = dates.first
     last  = dates.last
 
-    if first.year == last.year && first.month == last.month
+    if first.year == last.year && first.month == last.month && first.day == last.day
+      l(first, format: :same_day)
+    elsif first.year == last.year && first.month == last.month
       l(first, format: :range_same_year_and_month, last_day: last.day)
     elsif first.year == last.year
       l(first, format: :range_same_year, last_day: l(last, format: :no_year))
