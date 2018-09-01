c324347c596fdbf13fd3254e19dfd082ca7da4a2
François Beausoleil <francois@teksol.info>
2018-09-01 02:28:48 -0400

    Improve reservations page UX by not scrolling back on save
    Instead of doing a full form submit and page redraw, only redraw the
    singel TR that holds the row that was just modified. This improves the
    user experience 100x because we can simply scroll down the list and add
    and remove products/instnaces to our event without losing our position
    on the view.

--- app/controllers/events/reservations_controller.rb
+++ app/controllers/events/reservations_controller.rb
@@ -31,25 +31,25 @@ class Events::ReservationsController < ApplicationController
     return redirect_to(action: :index) if params[:products].blank?
 
     current_group.transaction do
-      products = current_group.products.includes(reservations: [:event, :instance]).where(slug: params[:products].keys).to_a
+      @products = current_group.products.includes(reservations: [:event, :instance]).where(slug: params[:products].keys).to_a
 
       if %i[ add remove lease lease_all return ].all?{|key| params[key].blank?}
         # NOP
       elsif params[:add].present?
-        @event.add(products, metadata: domain_event_metadata)
-        flash[:notice] = t(:added, scope: "events.reservations.create", num: products.size)
+        @event.add(@products, metadata: domain_event_metadata)
+        flash[:notice] = t(:added, scope: "events.reservations.create", num: @products.size)
       elsif params[:remove].present?
-        @event.remove(products, metadata: domain_event_metadata)
-        flash[:notice] = t(:removed, scope: "events.reservations.create", num: products.size)
+        @event.remove(@products, metadata: domain_event_metadata)
+        flash[:notice] = t(:removed, scope: "events.reservations.create", num: @products.size)
       elsif params[:lease].present?
-        @event.lease(products, metadata: domain_event_metadata)
-        flash[:notice] = t(:leased, scope: "events.reservations.create", num: products.size)
+        @event.lease(@products, metadata: domain_event_metadata)
+        flash[:notice] = t(:leased, scope: "events.reservations.create", num: @products.size)
       elsif params[:lease_all].present?
         @event.lease_all(metadata: domain_event_metadata)
-        flash[:notice] = t(:leased_all, scope: "events.reservations.create", num: products.size)
+        flash[:notice] = t(:leased_all, scope: "events.reservations.create", num: @products.size)
       elsif params[:return].present?
-        @event.return(products, metadata: domain_event_metadata)
-        flash[:notice] = t(:returned, scope: "events.reservations.create", num: products.size)
+        @event.return(@products, metadata: domain_event_metadata)
+        flash[:notice] = t(:returned, scope: "events.reservations.create", num: @products.size)
       else
         raise "ASSERTION ERROR: One of add and remove were supposed to be filled in, none were?!?"
       end
@@ -57,10 +57,22 @@ class Events::ReservationsController < ApplicationController
       @event.save!
     end
 
-    if params[:back_to_event].blank?
-      redirect_to action: :index
-    else
-      redirect_to event_path(@event)
+    respond_to do |format|
+      format.html do
+        if params[:back_to_event].blank?
+          redirect_to action: :index
+        else
+          redirect_to event_path(@event)
+        end
+      end
+
+      format.js do
+        @double_booked_products = Product.double_booked(@event).to_set
+        # We must reload products because we worked on the event's products instead
+        # This is a limitation of the ActiveRecord implementation
+        @products = current_group.products.includes(reservations: [:event, :instance]).where(slug: params[:products].keys).to_a
+        render
+      end
     end
   end
 
