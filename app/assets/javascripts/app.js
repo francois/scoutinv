36ec727e457e0af45bbf62f61f7bf6cab6a25ae4
François Beausoleil <francois@teksol.info>
2018-09-05 10:13:56 -0400

    Visually indicate that the image is rotating
    This improves UX because the end-user knows their click was registered.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,9 @@
 $(document).foundation()
+$(document).on("turbolinks:load", function(){
+  document.body.addEventListener("ajax:beforeSend", function(event){
+    var url = event.detail[1].url
+    var id = url.replace(/\//g, "-").replace(/^-products-/, "product-").replace(/images-/, "image-").replace(/-(left|right)$/, "");
+    $("#" + id + " form").remove();
+    $("#" + id).addClass("connecting");
+  });
+});
