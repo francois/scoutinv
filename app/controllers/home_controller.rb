1ea5abd6f74da3e2cddec0c0146850601855f285
François Beausoleil <francois@teksol.info>
2018-11-11 15:54:33 -0500

    Don't pollute the home page with so many old events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < PublicController
     if member_signed_in?
       @page_title = I18n.t("page_title", scope: "home")
       @products   = current_group.products.not_recently_reserved.limit(10)
-      @events     = current_group.events.after(2.weeks.ago)
+      @events     = current_group.events.after(10.days.ago)
       render action: "authenticated"
     else
       @page_title = nil
