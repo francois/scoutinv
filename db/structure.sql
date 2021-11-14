653d02a1065b5a46f8966e68b7a7cc73c990dbdf
François Beausoleil <francois@teksol.info>
2021-11-14 10:12:41 -0500

    bundle update

--- db/structure.sql
+++ db/structure.sql
@@ -5,6 +5,7 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
@@ -53,8 +54,6 @@ $$;
 
 
 SET default_tablespace = '';
-
-SET default_with_oids = false;
 
 --
 -- Name: que_jobs; Type: TABLE; Schema: public; Owner: -
