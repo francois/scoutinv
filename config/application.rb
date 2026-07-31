f3a6ddd9964aae3de69e5fdf8d86f16176b36187
François Beausoleil <francois@inspectify.com>
2026-07-30 23:01:01 -0400

    Upgrade Rails framework defaults to 6.0

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ WEB_IMAGE_CONFIG = {
 module Scoutinv
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     config.autoloader = :zeitwerk
 
     # Settings in config/environments/* take precedence over those specified here.
