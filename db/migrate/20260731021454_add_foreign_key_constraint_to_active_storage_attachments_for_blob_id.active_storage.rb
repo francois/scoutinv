6ec3257b5970bc7e459624ad44d6b3ecabf7e17c
François Beausoleil <francois@inspectify.com>
2026-08-03 14:48:51 -0400

    Remove orphaned Active Storage attachments (#24)

--- db/migrate/20260731021454_add_foreign_key_constraint_to_active_storage_attachments_for_blob_id.active_storage.rb
+++ db/migrate/20260731021454_add_foreign_key_constraint_to_active_storage_attachments_for_blob_id.active_storage.rb
@@ -4,6 +4,15 @@ class AddForeignKeyConstraintToActiveStorageAttachmentsForBlobId < ActiveRecord:
     return if foreign_key_exists?(:active_storage_attachments, column: :blob_id)
 
     if table_exists?(:active_storage_blobs)
+      execute <<~SQL
+        DELETE FROM active_storage_attachments
+        WHERE NOT EXISTS (
+          SELECT 1
+          FROM active_storage_blobs
+          WHERE active_storage_blobs.id = active_storage_attachments.blob_id
+        )
+      SQL
+
       add_foreign_key :active_storage_attachments, :active_storage_blobs, column: :blob_id
     end
   end
